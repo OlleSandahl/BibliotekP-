93ed089c5761f6b146a9fbea0c621f8a242cbe66
olle sandahl <olle.sandahl@live.se>
2020-02-05 14:12:30 +0100

    Lade till Ytterligarekod

---
+++ Business Layer/DataStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business_Layer.Interfaces;
+using Business_Layer.Enums;
+
+namespace Business_Layer
+{
+    public class DataStore
+    {
+        private IInventory inventory = null;
+        private IList<IMember> members;
+        private IList<ILibrarian> librarians;
+        private IList<IBook> books;
+        private IList<IBook> Availabebooks;
+        private IList<IBook> BookedBooks;
+        private IList<IBooking> bookings;
+        private IList<IInvoice> invoices;
+
+
+
+    }
+}
