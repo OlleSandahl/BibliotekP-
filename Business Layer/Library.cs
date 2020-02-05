633d2e1aedc2811101e58e95f79232ac8c4b67ed
olle sandahl <olle.sandahl@live.se>
2020-02-05 17:22:10 +0100

    ll

--- Business Layer/Library.cs
+++ Business Layer/Library.cs
@@ -11,5 +11,51 @@ namespace Business_Layer
 {
     class Library
     {
+        private IDataStore datastore = null;
+
+        public Library(IDictionary<string, string> settings)
+        {
+            datastore = new DataStore(settings);
+        }
+
+        public ILogIn LogingIn(int libNr, string password)
+        {
+            ILogIn loggaIn = null;
+            ILibrarian librarian = datastore.GetLibrarianNr(libNr);
+            if (librarian != null && librarian.CorrectPassword(password))
+                loggaIn = new LogIn(librarian);
+            return loggaIn;
+        }
+
+        public IList<IBook> GetAvailabeBooks()
+        {
+            return datastore.GetAvailabeBooks();
+        }
+        public IList<IBooking> GetBookings()
+        {
+            return datastore.GetBookings();
+        }
+        public IList<IBook> GetReservedBooks(int bookingNr)
+        {
+            return datastore.GetReservedBooks(bookingNr);
+        }
+        public IBook GetBookFromISBN(int isbn)
+        {
+            return datastore.GetBookFromISBN(isbn);
+        }
+        public IInvoice ReturnBook(int bookingNr)
+        {
+            return datastore.ReturnBook(bookingNr);
+        }
+        public IBooking ReservBook(IList<IBook> books, int libNr, int memberNr)
+        {
+            return datastore.ReservBook(books, libNr, memberNr);
+        }
+
+
+
+
+
+        
     }
 }
