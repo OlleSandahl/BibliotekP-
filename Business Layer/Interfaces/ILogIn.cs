3cb75115fc4ed4e5feb5100bf93b103924156475
olle sandahl <olle.sandahl@live.se>
2020-02-05 16:49:53 +0100

    Loggin

---
+++ Business Layer/Interfaces/ILogIn.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business_Layer.Enums;
+using Business_Layer.Interfaces;
+
+namespace Business_Layer
+{
+    public interface ILogIn
+    {
+        ILibrarian Librarian { get; set; }
+    }
+}
---
+++ Business Layer/Library.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business_Layer.Enums;
+using Business_Layer.Interfaces;
+using Business_Layer.Exceptions;
+
+namespace Business_Layer
+{
+    class Library
+    {
+    }
+}
