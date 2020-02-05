60e541e17c7dc182be7e8a9db53b8ec79e558910
Gabriel Lundberg Puglia <s174099@student.hb.se>
2020-02-05 16:50:23 +0100

    Meny

---
+++ ConsolePresentation/States/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePresentation
+{
+    public class MainMenu
+    {
+        public void ShowMenu
+        {
+            Console.Clear();
+            Console.WriteLine("MainMenu\n");
+            Console.WriteLine("1. Boka bok");
+            Console.WriteLine("2. Återlämna bok");
+            Console.WriteLine("3. Ta bort bokning");
+            Console.WriteLine("4. Exit\n");
+            Console.Write("Val: ");
+
+            int choice;
+            int.TryParse(Console.ReadLine(), out choice);
+        }
+    }
+}
